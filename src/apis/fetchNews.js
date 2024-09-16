612915b6fedda5d531d63b5eba171d12a4655c86
Madhav Varma <160639800+bMadhavvarma@users.noreply.github.com>
2024-09-16 18:15:20 +0530

    Update fetchNews.js

--- src/apis/fetchNews.js
+++ src/apis/fetchNews.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const fetchNews = async () => {
-  const requrl = "https://newsapi.org/v2/everything?q=tesla&from=2024-08-15&sortBy=publishedAt&apiKey=1a8459d107684d2b842767b8ab805286";
+  const requrl = "https://newsapi.org/v2/everything?q=apple&from=2024-09-15&to=2024-09-15&sortBy=popularity&apiKey=1a8459d107684d2b842767b8ab805286";
   
   try {
     const response = await axios.get(requrl);

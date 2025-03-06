aa8b470b49daf1b15a1672cf8954f404e4722bf4
Madhav Varma <160639800+bMadhavvarma@users.noreply.github.com>
2025-03-06 08:13:20 +0530

    Update Weather.jsx

--- src/Components/Weather/Weather.jsx
+++ src/Components/Weather/Weather.jsx
@@ -1,32 +1,34 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Weather.module.css";
-import vector from "../../assets/Vector.png"
-import windspeed from "../../assets/windspeed.png"
-import group from "../../assets/Group.png"
+import vector from "../../assets/Vector.png";
+import windspeed from "../../assets/windspeed.png";
+import group from "../../assets/Group.png";
 import axios from "axios";
 
 function Weather() {
-    const [DateTime,setDateTime]=useState("");
-    const [condition,setCondition]=useState("");
-    const [ConditionIcon,setConditionIcon]=useState("");
-    const [Temperature,setTemperature]=useState("");
-    const [Pressure,setpressure]=useState("");
-    const [wind,setWind]=useState("");
-    const [humidity,setHumidity]=useState("");
+  const [dateTime, setDateTime] = useState("");
+  const [condition, setCondition] = useState("");
+  const [conditionIcon, setConditionIcon] = useState("");
+  const [temperature, setTemperature] = useState("");
+  const [pressure, setPressure] = useState("");
+  const [wind, setWind] = useState("");
+  const [humidity, setHumidity] = useState("");
+
   const fetchWeather = async () => {
     const requrl =
-      "http://api.weatherapi.com/v1/current.json?key=326abce5c6304ea2bc470307241509&q=Nellore";
+      "https://api.weatherapi.com/v1/current.json?key=326abce5c6304ea2bc470307241509&q=Nellore";
 
     try {
       const response = await axios.get(requrl);
+      console.log("Weather Data:", response.data); // Debugging
+
       setDateTime(response.data.location.localtime);
       setCondition(response.data.current.condition.text);
-      setConditionIcon(response.data.current.condition.icon);
+      setConditionIcon("https:" + response.data.current.condition.icon); // Fixed Icon URL
       setTemperature(response.data.current.temp_c);
-      setpressure(response.data.current.pressure_mb);
+      setPressure(response.data.current.pressure_mb);
       setWind(response.data.current.wind_kph);
       setHumidity(response.data.current.humidity);
-
     } catch (error) {
       console.error("Error fetching weather:", error);
     }
@@ -34,34 +36,46 @@ function Weather() {
 
   useEffect(() => {
     fetchWeather();
+    const interval = setInterval(fetchWeather, 60000); // Refresh every minute
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <div className={styles.container}>
-      <div className={styles.dateTime}>{DateTime}</div>
+      <div className={styles.dateTime}>{dateTime}</div>
+
       <div className={styles.bottomSection}>
+        {/* Weather Condition */}
         <div className={styles.bottomSectionElements}>
-            <img src={ConditionIcon} alt="condition icon" />
-            <p>{condition}</p>
+          <img src={conditionIcon} alt="Weather Condition" />
+          <p>{condition}</p>
         </div>
+
         <span>|</span>
+
+        {/* Temperature & Pressure */}
         <div className={styles.bottomSectionElements}>
-        <p>{Temperature}°C</p>
-        <div style={{display:"flex"}}>
-            <img src={vector} alt="vector" style={{height:"20px", marginTop:"12px"}}/>
-            <p style={{opacity:"0.7" , fontSize:"15px",padding:"5px"}}>{Pressure} mbar<br/>Pressure</p>
+          <p>{temperature}°C</p>
+          <div style={{ display: "flex" }}>
+            <img src={vector} alt="Pressure Icon" style={{ height: "20px", marginTop: "12px" }} />
+            <p style={{ opacity: "0.7", fontSize: "15px", padding: "5px" }}>
+              {pressure} mbar<br />Pressure
+            </p>
+          </div>
         </div>
-        </div>
+
         <span>|</span>
+
+        {/* Wind & Humidity */}
         <div className={styles.bottomSectionElements}>
-        <div style={{display:"flex",marginTop:"0px"}}>
-            <img src={windspeed} style={{height:"15px",marginTop:"5px",marginRight:"5px"}}/>
-               <p style={{opacity:"0.8" ,fontSize:"11px"}}>{wind} km/h<br/>Wind</p>
-            </div>
-            <div style={{display:"flex",marginTop:"10px"}}>
-            <img src={group} style={{height:"15px",marginTop:"5px",marginRight:"15px"}}/>
-               <p style={{opacity:"0.8" ,fontSize:"11px"}}>{humidity}<br/>humidity</p>
-            </div>
+          <div style={{ display: "flex", marginTop: "0px" }}>
+            <img src={windspeed} alt="Wind Speed Icon" style={{ height: "15px", marginTop: "5px", marginRight: "5px" }} />
+            <p style={{ opacity: "0.8", fontSize: "11px" }}>{wind} km/h<br />Wind</p>
+          </div>
+          <div style={{ display: "flex", marginTop: "10px" }}>
+            <img src={group} alt="Humidity Icon" style={{ height: "15px", marginTop: "5px", marginRight: "15px" }} />
+            <p style={{ opacity: "0.8", fontSize: "11px" }}>{humidity}%<br />Humidity</p>
+          </div>
         </div>
       </div>
     </div>
